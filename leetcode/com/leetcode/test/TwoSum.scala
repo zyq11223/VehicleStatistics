035a1e28a33f39e5a28b54034b61b6b70b92b7c9
yqs <411012285@qq.com>
2018-01-17 17:24:25 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Machine Learning/com/datascience/kmeans/RunKMeans.scala
    #	pom.xml

--- leetcode/com/leetcode/test/TwoSum.scala
+++ leetcode/com/leetcode/test/TwoSum.scala
@@ -1,15 +1,21 @@
 package com.leetcode.test
 
+import scala.collection.immutable.HashMap
+
 /**
   * Created by yqs on 2018/1/16.
   */
 object TwoSum {
   def twoSum(nums: Array[Int], target: Int): Array[Int] = {
-    val len = nums.length
-    val array = new Array[Int](len)
-    array
+   val result = new Array[Int](2)
+    val map = new HashMap[Int,Int]()
+    for (num<-nums)
+      {
+
+      }
+    result
   }
-  
+
   def main(args: Array[String]) {
 
   }
