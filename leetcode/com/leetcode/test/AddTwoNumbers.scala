743eab23a28ac7e1988c8b1447ed8babecc21658
yqs <411012285@qq.com>
2018-01-18 23:12:53 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Machine Learning/com/datascience/kmeans/RunKMeans.scala
    #	pom.xml

--- leetcode/com/leetcode/test/AddTwoNumbers.scala
+++ leetcode/com/leetcode/test/AddTwoNumbers.scala
@@ -6,11 +6,11 @@ package com.leetcode.test
   * Created by yqs on 2018/1/18.
   */
 object AddTwoNumbers {
+
   class ListNode(var _x: Int = 0) {
     var next: ListNode = null
     var x: Int = _x
   }
-
   def isIntByRegex(s : String) = {
     val pattern = """^(\d+)$""".r
     s match {
@@ -18,7 +18,48 @@ object AddTwoNumbers {
       case _ => false
     }
   }
-  def pow(x: Int, y: Int):Int = if(y-1>=0) x* pow(x,y-1)else return 1
+  def pow(x: Int, y: Int):Int = if(y>=1) x* pow(x,y-1) else return 1
+
+//  char *addBigInter(const char *a,const char *b) {
+//    char *result = NULL;
+//    int i, j, resultLen;
+//    int len1 = strlen(a);
+//    int len2 = strlen(b);
+//    resultLen = (len1 > len2) ? len1: len2;
+//    result = (char *)malloc((resultLen+2)*sizeof(char));
+//    memset(result, '0', (resultLen+1)*sizeof(char));
+//    result[resultLen+1] = '\0';
+//    int carry = 0, temp;
+//    for(i=len1-1, j=len2-1; i>=0 || j>=0; i--, j--, resultLen--)
+//    {
+//      temp = carry;
+//      if(i>=0) temp += a[i] - '0';
+//      if(j>=0) temp += b[j] - '0';
+//      if (temp >= 10) {
+//        result[resultLen] = temp - 10 + '0';
+//        carry = 1;
+//      } else {
+//        result[resultLen] = temp + '0';
+//        carry = 0;
+//      }
+//    }
+//    result[resultLen] = carry + '0';
+//    return result;
+//  }
+
+
+//  def addBigInter(a:String,b:String) :String ={
+//     val aChar = convert(a)
+//     val bChar =  convert(b)
+//
+//
+//  }
+
+  def convert(s:String):Array[Char] = {
+    val aChar = new Array[Char](s.length)
+    for (i<- 0 until s.length)  aChar(i) = s.charAt(i)
+    aChar
+  }
 
   def addTwoNumbers(l1: ListNode, l2: ListNode): ListNode = {
     val sum = toInt(l1) + toInt(l2)
@@ -29,7 +70,7 @@ object AddTwoNumbers {
       val c = sumString.charAt(i).toString
       if(isIntByRegex(c)){
         linkList(j) = new ListNode(Integer.parseInt(sumString.charAt(i).toString))
-        j += 1
+        j = j+ 1
       }
     }
     for (i <- 0 until linkList.length) {
@@ -46,24 +87,24 @@ object AddTwoNumbers {
     var temp = l
     var nums = 0
     var i = 0
-    while (temp!= null  && temp.next != null) {
+    while (temp!= null  &&( temp._x != 0||temp.next != null)) {
       nums += temp._x * pow(10,i)
       i = i+1
       temp = temp.next
       }
-    println(nums)
     nums
   }
 
   def main(args: Array[String]) {
-    val listNode1 = new ListNode(9)
-    val listNode2 = Array[Int](1, 9, 9, 9, 9, 9, 9, 9, 9, 9)
-    val linkList = new Array[ListNode](listNode2.length)
-    for (i <- 0 to 9) linkList(i) = new ListNode(listNode2(i))
+    val listNode1 = Array[Int](9)
+    val linkList1 = new Array[ListNode](listNode1.length)
+    for (i <- 0 to listNode1.length - 1) linkList1(i) = new ListNode(listNode1(i))
+    for (i <- 0 until listNode1.length) if (i + 1 <= linkList1.length - 1) linkList1(i).next = linkList1(i + 1)
 
-    for (i <- 0 until listNode2.length) {
-      if (i + 1 <= linkList.length - 1) linkList(i).next = linkList(i + 1)
-    }
-    addTwoNumbers(listNode1, linkList(0))
+    val listNode2 = Array[Int](1, 9, 9, 9, 9, 9, 9, 9, 9, 9)
+    val linkList2 = new Array[ListNode](listNode2.length)
+    for (i <- 0 to listNode2.length - 1) linkList2(i) = new ListNode(listNode2(i))
+    for (i <- 0 until listNode2.length) if (i + 1 <= linkList2.length - 1) linkList2(i).next = linkList2(i + 1)
+    addTwoNumbers(linkList1(0), linkList2(0))
   }
 }
